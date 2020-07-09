b9330ec4ead469f4402b9da8124caa0f40ef371b
qbob1 <bullock.quentin@gmail.com>
2020-07-08 23:09:59 -0400

    Added Validation to customer insertion

--- App.Web/Models/ViewModels/CustomerVM.cs
+++ App.Web/Models/ViewModels/CustomerVM.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Web.Models.ViewModels
 {
     public class CustomerVM
     {
+        [StringLength(60, MinimumLength = 3)]
+        [RegularExpression(@"^[a-z ,.'-]+$", ErrorMessage = "A First Name cannot contain \\ / : * ? \" < > | or numbers")]
+        [Required(ErrorMessage = "A First Name Is Required")]
         [DisplayName("First Name")]
         public String FirstName { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression(@"^[a-z ,.'-]+$", ErrorMessage = "A Last Name cannot contain \\ / : * ? \" < > | or numbers")]
         [DisplayName("Last Name")]
+        [Required(ErrorMessage = "A Last Name Is Required")]
         public String LastName { get; set; }
         [DisplayName("Location")]
         public string LocationName { get; set; }
